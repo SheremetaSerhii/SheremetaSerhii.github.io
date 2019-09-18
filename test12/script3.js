ef8e1d80819de809d8789ce2b877aeeeb79807f0
Sheremeta Serhii <sheremetaserhii@gmail.com>
2019-09-18 13:45:04 +0300

    word

--- test12/script3.js
+++ test12/script3.js
@@ -1,5 +1,5 @@
 function change(word) {
-    var newWord = new Array();
+    //var newWord = new Array();
     var sameSymbol = {
         i: "і",
         n: "п",
@@ -8,7 +8,7 @@ function change(word) {
         o: "о",
         c: "с"
     }
-    for (var i = 0; i < word.length; i++) {
+    /*for (var i = 0; i < word.length; i++) {
         if (sameSymbol.hasOwnProperty(word[i])) {
             newWord[i] = sameSymbol[word[i]];
         }
@@ -16,7 +16,23 @@ function change(word) {
             newWord[i] = word[i];
         }
     }
+    return newWord.join("");*/
+    /////////////////// NEW ///////////////////
+    newWord = word.split("");
+    console.log(newWord);
+    //newWord.map(function(item) {return item = item + "1"});
+    newWord.map(function(item) { return item += "1"});
+    // for (var i = 0; i < newWord.length; i++) {
+    //     newWord[i] += "1";
+    // }
+    console.log(newWord);
+    //var newWord = new Array();
+    //newWord = word.map(function { return "1" });
     return newWord.join("");
 }
 
-console.log(change("function"));
+//console.log(change("function"));
+
+word = "function".split("");
+word.map(function(item) { return item += "1"});
+console.log(word);
