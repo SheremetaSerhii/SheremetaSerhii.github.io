a9a8f80c3d614f8cdafeeca406a4167a9c2612ed
Sheremeta Serhii <sheremetaserhii@gmail.com>
2019-09-18 14:04:43 +0300

    word2

--- test12/script3.js
+++ test12/script3.js
@@ -17,22 +17,11 @@ function change(word) {
         }
     }
     return newWord.join("");*/
-    /////////////////// NEW ///////////////////
-    newWord = word.split("");
-    console.log(newWord);
-    //newWord.map(function(item) {return item = item + "1"});
-    newWord.map(function(item) { return item += "1"});
-    // for (var i = 0; i < newWord.length; i++) {
-    //     newWord[i] += "1";
-    // }
-    console.log(newWord);
-    //var newWord = new Array();
-    //newWord = word.map(function { return "1" });
-    return newWord.join("");
+    return word.split("").map(function (item) { if (sameSymbol.hasOwnProperty(item)) { return sameSymbol[item] } else { return item } }).join("");
 }
 
-//console.log(change("function"));
+console.log(change("function"));
 
-word = "function".split("");
-word.map(function(item) { return item += "1"});
-console.log(word);
+// word = "function".split("");
+// word = word.map(function(item) { return item += "1"});
+// console.log(word);
