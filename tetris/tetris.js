e0a4fe2498ccf2abc40f0773af0953cb6ef6acf2
Sheremeta Serhii <sheremetaserhii@gmail.com>
2019-12-04 02:42:51 +0200

    tetris

---
+++ tetris/tetris.js
@@ -0,0 +1,25 @@
+const XX = true;
+const OO = false;
+const FIGURES = {
+    "f1": {
+        "variationsQuantity": 2,
+        "v0": [
+            [OO, OO, OO, OO],
+            [OO, OO, OO, OO],
+            [OO, OO, OO, OO],
+            [OO, OO, OO, OO],
+        ],
+        "v1": [
+            [XX, XX, OO, OO],
+            [OO, XX, XX, OO],
+            [OO, OO, OO, OO],
+            [OO, OO, OO, OO],
+        ],
+        "v2": [
+            [OO, XX, OO, OO],
+            [XX, XX, OO, OO],
+            [XX, OO, OO, OO],
+            [OO, OO, OO, OO],
+        ]
+    },
+};
