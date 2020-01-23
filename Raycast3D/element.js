a533e93d1082a297bb6b227ac3bfc2e6624f6814
Sheremeta Serhii <sheremetaserhii@gmail.com>
2020-01-23 17:41:30 +0200

    raycast 3

--- Raycast3D/element.js
+++ Raycast3D/element.js
@@ -64,7 +64,7 @@ export class MovableElement extends GameElement {
     }
 
     setMoveSpeed(speed) {
-        _moveSpeed = speed;
+        this._moveSpeed = speed;
     }
 
     turnLeft(turnAngle) {
